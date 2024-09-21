62514f8e071514a6d82edaa152b9435c02cd0126
dip8080 <dipmollik7@gmail.com>
2024-09-21 08:51:20 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/sportyo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sportyo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Sprotyo extends StatelessWidget {
+  const Sprotyo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
