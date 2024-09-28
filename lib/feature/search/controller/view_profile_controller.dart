898937ad0234fc75349bde343ad79f07e7bffbce
sagor samadder <sagorsamadder51@gmail.com>
2024-09-28 09:58:37 +0600

    view profile

--- lib/feature/search/controller/view_profile_controller.dart
+++ lib/feature/search/controller/view_profile_controller.dart
@@ -1,25 +1,121 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
-import 'package:sportyo/feature/chat/screen/chats_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
 
-class PrpfoleDetailsController extends GetxController {
-  var sports = [
-    {"sport": "Football", "level": "Beginner"},
-    {"sport": "Basketball", "level": "Intermediate"},
-    {"sport": "Tennis", "level": "Advanced"},
-  ].obs;
+import '../model/profile_model.dart';
 
-  var languages = ["English", "Spanish", "French"].obs;
+class UsersController extends GetxController {
+  UsersController({required this.userId});
 
-  var upcomingEvents = ["Event 1", "Event 2", "Event 3"].obs;
+  final String userId;
+  @override
+  void onInit() {
+    super.onInit();
+    fetchUserData();
+  }
 
-  void startChat() {
-    Get.to(
-      () => ChatScreen(
-        name: "Tom",
-        image:
-            "https://img.freepik.com/free-photo/happy-business-man-standing-smiling-against-red-wall_155003-9367.jpg",
-        chatId: "5665569584784",
-      ),
-    );
+  // Observable list for sports
+  var sports = <SportsDetail>[].obs;
+
+  // Observable list for languages
+  var languages = <String>[].obs;
+
+  // Observable variable for the user's age
+  var age = 0.obs;
+
+  var userModel = UserModel(
+    id: '',
+    email: '',
+    userProfileImage: '',
+    firstName: null,
+    lastName: null,
+    gender: '',
+    dateOfBirth: '',
+    bio: '',
+    language: '',
+    incognito: false,
+    genderRestriction: false,
+    role: '',
+    userStatus: '',
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
+    sportsDetails: [],
+  ).obs;
+
+  // Method to calculate the age
+  int calculateAge(String dateOfBirth) {
+    DateFormat dateFormat = DateFormat('dd/MM/yyyy');
+    DateTime dob = dateFormat.parse(dateOfBirth);
+    DateTime currentDate = DateTime.now();
+
+    int userAge = currentDate.year - dob.year;
+
+    // Check if the birthday has occurred this year or not
+    if (currentDate.month < dob.month ||
+        (currentDate.month == dob.month && currentDate.day < dob.day)) {
+      userAge--;
+    }
+
+    return userAge;
+  }
+
+  Future<void> fetchUserData() async {
+    try {
+      // Retrieve userId and token from SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? userId = prefs.getString('userId');
+      String? token = prefs.getString('token');
+
+      if (userId == null || token == null) {
+        return;
+      }
+
+      // API endpoint
+      String url = 'https://sports-app-alpha.vercel.app/api/v1/users/$userId';
+
+      // Making the HTTP GET request
+      var response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Authorization': token,
+        },
+      );
+      if (kDebugMode) {
+        print(response.body);
+      }
+      if (response.statusCode == 200) {
+        var jsonResponse = json.decode(response.body);
+
+        // Update the userModel with the data from the response
+        userModel.value = UserModel.fromJson(jsonResponse['data']);
+
+        // Update the sports observable list
+        sports.value = userModel.value.sportsDetails;
+
+        if (userModel.value.language.isNotEmpty) {
+          languages.value = userModel.value.language
+              .split(',')
+              .map((lang) => lang.trim())
+              .toList();
+        } else {
+          languages.clear();
+        }
+
+        if (userModel.value.dateOfBirth.isNotEmpty) {
+          age.value = calculateAge(userModel.value.dateOfBirth);
+        }
+      } else {
+        if (kDebugMode) {
+          print('Failed to load user data: ${response.statusCode}');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error: $e');
+      }
+    } finally {}
   }
 }
--- lib/feature/search/screen/view_profile_page.dart
+++ lib/feature/search/screen/view_profile_page.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sportyo/core/const/app_colors.dart';
+import 'package:sportyo/core/const/image_path.dart';
 import 'package:sportyo/feature/profile/widget/global_text_style.dart';
-
 import '../controller/view_profile_controller.dart';
+import '../widget/profile_loading.dart';
 
-class PartnerDetailsPage extends StatelessWidget {
-  const PartnerDetailsPage({super.key});
+class UserDetailsPage extends StatelessWidget {
+  final String userId;
+  const UserDetailsPage({super.key, required this.userId});
 
   @override
   Widget build(BuildContext context) {
-    final PrpfoleDetailsController controller =
-        Get.put(PrpfoleDetailsController());
+    final UsersController controller = Get.put(UsersController(userId: userId));
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -23,191 +24,203 @@ class PartnerDetailsPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Circular Image
-              const Center(
-                child: CircleAvatar(
-                  radius: 75,
-                  backgroundImage: NetworkImage(
-                      'https://img.freepik.com/free-photo/happy-business-man-standing-smiling-against-red-wall_155003-9367.jpg'),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Name and Age
-              const Center(
-                child: Text(
-                  'Tom, Age 25',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    controller.startChat();
-                  },
-                  child: Text(
-                    'Start a discussion',
-                    style: globalTextStyle(
-                      color: AppColors.whiteColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-
-              // Sports Section
-              const Text(
-                'Sports',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 16),
-
-              // List of Sports
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: controller.sports.length,
-                itemBuilder: (context, index) {
-                  final sport = controller.sports[index];
-                  return InkWell(
-                    child: Container(
-                      margin: const EdgeInsets.only(bottom: 13),
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                          color: const Color(0xff010101),
-                        ),
+      body: Obx(() => controller.userModel.value.id.isNotEmpty
+          ? Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Circular Image (Using dynamic profile image)
+                    Center(
+                      child: CircleAvatar(
+                        radius: 75,
+                        backgroundImage:
+                            controller.userModel.value.userProfileImage == '' ||
+                                    controller
+                                            .userModel.value.userProfileImage ==
+                                        null
+                                ? const AssetImage(ImagePath.profile)
+                                : NetworkImage(
+                                    controller.userModel.value.userProfileImage
+                                        .toString(),
+                                  ),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 13),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("${sport['sport']}"),
-                            Container(
-                              height: 25,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: const Color(0xff010101),
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Name and Age
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          controller.userModel.value.firstName ?? '',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        // ignore: unnecessary_null_comparison
+                        controller.userModel.value.dateOfBirth == null ||
+                                controller.userModel.value.dateOfBirth == ""
+                            ? const Text('')
+                            : Text(
+                                ' ,${controller.age} years',
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                borderRadius: BorderRadius.circular(10),
                               ),
-                              child: Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10),
-                                  child: Text("${sport['level']}"),
-                                ),
-                              ),
-                            )
-                          ],
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // controller.startChat();
+                        },
+                        child: Text(
+                          'Start a discussion',
+                          style: globalTextStyle(
+                            color: AppColors.whiteColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
                     ),
-                  );
-                },
-              ),
-              const SizedBox(height: 24),
+                    const SizedBox(height: 24),
 
-              // Biography Section
-              const Text(
-                'Bio',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'I’m an everyday person who enjoys life’s simple pleasures. I work hard, spend time with family, and like to relax with a good movie or book. I enjoy morning walks, local events, and helping neighbors when I can. I believe in kindness and staying positive.',
-                style: TextStyle(fontSize: 14),
-              ),
-              const SizedBox(height: 24),
-
-              // Languages Section
-              const Text(
-                'Languages',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 16),
-
-              // List of Languages
-              Wrap(
-                spacing: 8.0,
-                runSpacing: 5.0,
-                children: controller.languages.map((language) {
-                  return Container(
-                    height: 30,
-                    decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
-                        vertical: 5,
-                      ),
-                      child: Text(
-                        language,
-                        style: globalTextStyle(fontSize: 12),
-                      ),
+                    // Sports Section
+                    const Text(
+                      'Sports',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 24),
+                    const SizedBox(height: 16),
 
-              // Location Section
-              const Text(
-                'Location',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'City XXX',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 24),
+                    // List of Sports
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: controller.sports.length,
+                      itemBuilder: (context, index) {
+                        final sport = controller.sports[index];
+                        return InkWell(
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 13),
+                            height: 40,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: const Color(0xff010101),
+                              ),
+                            ),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 13),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(sport.sportsName),
+                                  Container(
+                                    height: 25,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: const Color(0xff010101),
+                                      ),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Center(
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10),
+                                        child: Text(sport.level),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 24),
 
-              // Upcoming Events Section
-              const Text(
-                'Upcoming Events',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 16),
+                    // Biography Section
+                    const Text(
+                      'Bio',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      controller.userModel.value.bio.isNotEmpty
+                          ? controller.userModel.value.bio
+                          : 'No bio available',
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                    const SizedBox(height: 24),
 
-              // List of Upcoming Events
-              Obx(
-                () => ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: controller.upcomingEvents.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 10),
-                      height: 30,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.black),
-                      ),
-                      child: Center(
-                        child: Text(controller.upcomingEvents[index]),
-                      ),
-                    );
-                  },
+                    // Languages Section
+                    const Text(
+                      'Languages',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 16),
+
+                    // List of Languages
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 5.0,
+                      children: controller.languages.map((language) {
+                        return Container(
+                          height: 30,
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 5),
+                            child: Text(
+                              language,
+                              style: globalTextStyle(fontSize: 12),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                    const SizedBox(height: 24),
+
+                    // Location Section (Placeholder)
+                    const Text(
+                      'Location',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'City XXX',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 24),
+
+                    const Text(
+                      'Upcoming Events',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            )
+          : profileShimmerLoadingEffect()),
     );
   }
 }
