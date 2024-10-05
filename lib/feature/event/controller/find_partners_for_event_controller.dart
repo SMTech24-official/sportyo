f4d44c420074ce2d1aec10a822f8f6a950371823
yasinpalash <yasinmiapolash@gmail.com>
2024-10-05 17:28:02 +0600

    completed find partners

--- lib/feature/event/controller/find_partners_for_event_controller.dart
+++ lib/feature/event/controller/find_partners_for_event_controller.dart
@@ -1,51 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sportyo/core/service_class/network_caller/repository/second_Network_caller.dart';
+import 'package:sportyo/feature/event/model/user_list_by_id.dart';
+import '../../../core/service_class/network_caller/utility/usrls.dart';
 
 class FindPartnersForEventController extends GetxController {
   // Search controller
   TextEditingController searchController = TextEditingController();
 
-  // Dummy user list
-  var users = [
-    {
-      'id': 1,
-      'name': 'Alice Johnson',
-      'age': 25,
-      'image':
-      'https://img.freepik.com/free-photo/happy-business-man-standing-smiling-against-red-wall_155003-9367.jpg',
-    },
-    {
-      'id': 2,
-      'name': 'Bob Smith',
-      'age': 30,
-      'image':
-      'https://img.freepik.com/free-photo/astronaut-wearing-space-suit-earth_23-2151650707.jpg?t=st=1727326292~exp=1727329892~hmac=9d7a6a622b377162314cb8ee6edde0e3a92bfc2c0c7dea1eefe6f67bc99f582e&w=1380',
-    },
-  ].obs;
+  final Rx<UserListByEventId> _userListById = UserListByEventId().obs;
+  Rx<UserListByEventId> get userListById => _userListById;
+  Future<void> getEventListByEventId(String eventId)async{
 
-  // Filtered user list
-  var filteredUsers = <Map<String, dynamic>>[].obs;
-
-  @override
-  void onInit() {
-    // Initialize filteredUsers with all users when the controller is initialized
-    filteredUsers.assignAll(users);
-    super.onInit();
-  }
-
-  // Function to filter users by name
-  void searchUsers(String query) {
-    if (query.isEmpty) {
-      // If the search query is empty, show all users
-      filteredUsers.assignAll(users);
-    } else {
-      // Filter users by name
-      filteredUsers.assignAll(
-        users.where((user) => user['name'].toString().toLowerCase().contains(query.toLowerCase())).toList(),
-      );
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var token = prefs.getString("token");
+    EasyLoading.show(status: "Loading...");
+    try {
+      final response = await SecondNetworkCaller().getRequest(Urls.findPartnersByEvent(eventId,),token: token);
+      // Handle response success
+      if (response.isSuccess) {
+        _userListById.value = UserListByEventId.fromJson(response.responseData);
+      }
+    } catch (error) {
+      EasyLoading.showError('Failed to add event. Please try again.');
+    } finally {
+      EasyLoading.dismiss();
     }
-  }
 
+  }
   @override
   void onClose() {
     searchController.dispose();
